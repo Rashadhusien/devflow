c311b49e1e6a1478028c23b64837b12e786019ab
Rashad Hussein <rashadhusein440@gmail.com>
2025-09-09 23:48:26 +0300

    implement Ask a Question page with QuestionForm component; add AskQuestionSchema for form validation

---
+++ components/forms/QuestionForm.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { AskQuestionSchema } from "@/lib/validations";
+
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+
+const QuestionForm = () => {
+  const form = useForm({
+    resolver: zodResolver(AskQuestionSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
+
+  const handleCreateQuestion = () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      >
+        <FormField
+          control={form.control}
+          name={"title"}
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col ">
+              <FormLabel className="paragraph-medium text-dark400_light700">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] rounded-1.5 border m-0"
+                />
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2 pl-1">
+                Be specific an imagine you&apos;re asking a question to another
+                person
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"content"}
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col ">
+              <FormLabel className="paragraph-medium text-dark400_light700">
+                Detailed explanation of your problem{" "}
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>{/* todo: editor componet */}</FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2 pl-1">
+                Introduce the problem and expand on what you&apos;ve put in the
+                title.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name={"tags"}
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col ">
+              <FormLabel className="paragraph-medium text-dark400_light700">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <div>
+                  <Input
+                    {...field}
+                    className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700 no-focus min-h-[56px] rounded-1.5 border m-0"
+                  />
+                  Tags
+                </div>
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2 pl-1">
+                Add up to 3 tags to describe what your question is about.{" "}
+                <strong>You need to press enter to add a tag.</strong>
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="mt-16 flex justify-end">
+          <Button
+            type="submit"
+            className="primary-gradient !text-light-900 cursor-pointer"
+          >
+            Ask A Question
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default QuestionForm;
--- lib/validations.ts
+++ lib/validations.ts
@@ -51,6 +51,22 @@ export const SignUpSchema = z.object({
     }),
 });
 
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is requiered." })
+    .max(100, { message: "Title cannot exceed 100 characters." }),
+  content: z.string().min(1, { message: "content is required." }),
+  tags: z.array(
+    z
+      .string()
+      .min(1, { message: "Tag is requiered" })
+      .max(30, { message: "Tag cannot exceed 30 characters." })
+      .min(1, { message: "At least one tag is requiered" })
+      .max(3, { message: "Cannot add more than 3 tags" })
+  ),
+});
+
 // export const AskQuestionSchema = z.object({
 //   title: z
 //     .string()
