1b987e951ad870eb80386c322574ae5e19308810
Rashad Hussein <rashadhusein440@gmail.com>
2025-09-09 23:58:07 +0300

    fix validation messages in AskQuestionSchema for clarity and correctness

--- lib/validations.ts
+++ lib/validations.ts
@@ -54,17 +54,18 @@ export const SignUpSchema = z.object({
 export const AskQuestionSchema = z.object({
   title: z
     .string()
-    .min(5, { message: "Title is requiered." })
+    .min(5, { message: "Title is required." })
     .max(100, { message: "Title cannot exceed 100 characters." }),
-  content: z.string().min(1, { message: "content is required." }),
-  tags: z.array(
-    z
-      .string()
-      .min(1, { message: "Tag is requiered" })
-      .max(30, { message: "Tag cannot exceed 30 characters." })
-      .min(1, { message: "At least one tag is requiered" })
-      .max(3, { message: "Cannot add more than 3 tags" })
-  ),
+  content: z.string().min(1, { message: "Content is required." }),
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required" })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
+    )
+    .min(1, { message: "At least one tag is required" })
+    .max(3, { message: "Cannot add more than 3 tags" }),
 });
 
 // export const AskQuestionSchema = z.object({
