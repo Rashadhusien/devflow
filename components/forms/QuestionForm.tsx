7f38629167b56e7fc4428818324961c3ca9856b1
rashad <rashadhusein440@gmail.com>
2025-10-15 23:08:05 +0300

    Create Answer Form

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -66,7 +66,8 @@ const QuestionForm = ({ question, isEdit = false }: Params) => {
             description: "Question updated successfully",
           });
 
-          if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+          if (result.data)
+            router.push(ROUTES.QUESTION(result.data._id as string));
         } else {
           toast.error(`Error: ${result.status}`, {
             description: result.error?.message || "Something went wronge",
