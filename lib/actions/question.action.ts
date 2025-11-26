8087787d749a4aec8301516bdceb9c232237c5c4
rashad <rashadhusein440@gmail.com>
2025-11-26 17:15:28 +0200

    Show top questions

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -20,6 +20,7 @@ import {
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
+import dbConnect from "../mongoose";
 import {
   AskQuestionSchema,
   EditQuestionSchema,
@@ -296,6 +297,24 @@ export async function IncrementViews(
     return {
       success: true,
       data: { views: question.views },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<ActionResponse<Q[]>> {
+  try {
+    await dbConnect();
+    const questions = await Question.find()
+      .sort({
+        views: -1,
+        upvotes: -1,
+      })
+      .limit(5);
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(questions)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
